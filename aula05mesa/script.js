c11dee70ea26fe5cc17c590bc313299e5c6cd3c1
Luiz Henrique <7henrique18@gmail.com>
2021-10-25 15:31:19 -0300

    fix: change function name

--- aula05mesa/script.js
+++ aula05mesa/script.js
@@ -1,6 +1,6 @@
 let theme = "light";
 
-const changeTheme = () => {
+const handleChangeTheme = () => {
   if (theme === "light") {
     document.body.style.backgroundColor = "#f1f1f1";
     document.querySelector("button").innerText = "🌞";
