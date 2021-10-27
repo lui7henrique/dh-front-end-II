fba2983d17eb119bbe7fcd40640eb277c676f478
Luiz Henrique <7henrique18@gmail.com>
2021-10-26 21:43:39 -0300

    feat: change readme

--- aula05mesa/script.js
+++ aula05mesa/script.js
@@ -4,6 +4,7 @@ const handleChangeTheme = () => {
   if (theme === "light") {
     document.body.style.backgroundColor = "#f1f1f1";
     document.querySelector("button").innerText = "🌞";
+
     theme = "dark";
   } else {
     document.body.style.backgroundColor = "#1e1e1e";
