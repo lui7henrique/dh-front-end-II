bf8502aa97775f921c259b772601530509452626
Luiz Henrique <7henrique18@gmail.com>
2021-11-18 15:41:21 -0300

    feat(aula17): atividade

--- aula16-mesa/js/script.js
+++ aula16-mesa/js/script.js
@@ -5,17 +5,19 @@ const button = document.querySelector("button");
 
 const handleGetData = async () => {
   const url = `https://reqres.in/api/users?page=${page}`;
+
   const { data } = await fetch(url).then((res) => res.json());
+  console.log(data);
 
   data.map((user) => {
     const li = document.createElement("li");
     li.innerHTML = `
-     <img src="${user.avatar}" alt="${user.first_name}">
-     <div>
-      <h2>${user.first_name} ${user.last_name}</h2>
-      <h5>${user.email}</h5>
-     </div>
-    `;
+      <img src="${user.avatar}" alt="${user.first_name}">
+      <div>
+       <h2>${user.first_name} ${user.last_name}</h2>
+       <h5>${user.email}</h5>
+      </div>
+     `;
 
     ul.appendChild(li);
   });
---
+++ aula17-atividade/js/script.js
@@ -0,0 +1,36 @@
+const url =
+  "http://ddragon.leagueoflegends.com/cdn/11.23.1/data/en_US/champion.json";
+
+const main = document.querySelector("main");
+
+const loadData = async () => {
+  const response = await fetch(url);
+  const { data } = await response.json();
+
+  const unformattedChampions = Object.values(data);
+  const champions = unformattedChampions.map((champion) => {
+    return {
+      name: champion.name,
+      title: champion.title,
+      image: `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`,
+    };
+  });
+
+  champions.map((champion) => {
+    const championCard = document.createElement("div");
+    championCard.classList.add("champion");
+    championCard.innerHTML = `
+      <img src="${champion.image}" alt="${champion.name}">
+      <div class="infos">
+        <h2>${champion.name}</h2>
+        <sub>${champion.title}</sub>
+      </div>
+    `;
+    main.appendChild(championCard);
+
+    console.log(championCard);
+  });
+
+  const button = document.querySelector("button");
+  button.style.display = "none";
+};
